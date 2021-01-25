8f8c80a48c08711aad54aa692456fb7d23d91221
ArcherN9 <>
2021-01-25 18:17:48 +0530

    Rationale:
    1. Redid the entire IAmRich application. It had been a while since I
       touched any of these.

--- IAmRich/lib/main.dart
+++ IAmRich/lib/main.dart
@@ -6,17 +6,28 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        // The top section of the Scaffold that carries the AppBar
+        // This is the section which carries the title of the application
         appBar: AppBar(
-          title: Text('I Am Rich'),
+          title: Text("I Am Rich"),
           backgroundColor: Colors.blueGrey[900],
-        ),
-        body: Scaffold(
-          backgroundColor: Colors.blueGrey[200],
-          body: Center(
-            child: Image(image: AssetImage('images/diamond.png')),
+        ), // App Bar
+
+        // The background color property of the scaffold applies the color to
+        // the scaffold in its entirety.
+        backgroundColor: Colors.blueGrey[700],
+
+        // The main content of the IAmRich application is a simple image
+        // taken from the course resources and displayed here.
+        // You should be able to check the image file in the resources.
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+            width: 220,
+            height: 220,
           ),
-        ),
-      ),
+        ), // Center
+      ), // Scaffold
     ),
   );
 }
