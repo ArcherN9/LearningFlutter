a0b034787fdd23c08b4b52e56724bd73132447a6
ArcherN9 <>
2021-01-28 13:02:28 +0530

    Rationale:
    1. Added an Application Icon
    2. Finishing changes to the MiCard application application.

--- MiCard/lib/main.dart
+++ MiCard/lib/main.dart
@@ -21,22 +21,104 @@ class MiCardApplication extends StatelessWidget {
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
             children: [
               CircleAvatar(
                 radius: 50,
-                backgroundImage: AssetImage('images/DP.jpg'),
+                backgroundImage: AssetImage('images/DP-2.JPG'),
               ), // Circle Avatar
               Container(
                 height: 20,
               ), // Empty container for height control
               Text(
-                'Daksh Srivastava',
+                'Archer N9',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   color: Colors.white,
                   fontSize: 35,
                   fontWeight: FontWeight.bold,
                 ), // Username
               ),
+              Text(
+                'Android Developer via Kotlin & Flutter',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.blueGrey.shade200,
+                    fontSize: 18,
+                    letterSpacing: 1.2,
+                    fontWeight: FontWeight.bold), // Description
+              ), // Description Text
+              SizedBox(
+                height: 20,
+                child: Divider(
+                  indent: 35,
+                  endIndent: 35,
+                  color: Colors.blueGrey.shade300,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ), // Sized box between Description and phone number
+              Card(
+                margin: EdgeInsets.only(left: 27, right: 27),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone_android,
+                    size: 25,
+                    color: Colors.blueGrey[900],
+                  ), // Phone icon
+                  title: Text(
+                    '+44 741 831 0977',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 16,
+                      color: Colors.blueGrey.shade900,
+                    ),
+                  ), // Phone number text
+                ),
+              ), // Phone number container
+              SizedBox(
+                height: 5,
+              ), // Sized box between Description and phone number
+              Card(
+                margin: EdgeInsets.only(left: 27, right: 27),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 25,
+                    color: Colors.blueGrey[900],
+                  ), // Phone icon
+                  title: Text(
+                    'divya.s90@yahoo.com',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 16,
+                      color: Colors.blueGrey.shade900,
+                    ),
+                  ), // Phone number text
+                ),
+              ), // Email container
+              SizedBox(
+                height: 5,
+              ), // Sized box between Description and phone number
+              Card(
+                margin: EdgeInsets.only(left: 27, right: 27),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.web,
+                    size: 25,
+                    color: Colors.blueGrey[900],
+                  ), // Phone icon
+                  title: Text(
+                    'https://archern9.github.io/',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 16,
+                      color: Colors.blueGrey.shade900,
+                    ), // Website Text Style
+                  ), // Website text
+                ), // Website List Title
+              ), // Website Card
               Container(
                 width: double.infinity,
               ) // Invisible container for central positioning on horizontal axis
