4a6204e4c93969f71c6cea6232c17f4b45876683
ArcherN9 <>
2021-03-11 21:17:31 +0530

    Rationale:
    1. Modified the Home Class to create a column with two section. One for
       the question and the other one to populate possible responses.

--- Quizzler/lib/Home/home.dart
+++ Quizzler/lib/Home/home.dart
@@ -5,25 +5,47 @@ import 'package:Quizzler/Widgets/AnswerTile.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 1,
+          child: TopQuestionSection(),
+        ),
+        Expanded(
+          flex: 1,
+          child: BottomAnswerSection(),
+        ),
+      ],
+    );
+  }
+}
+
+class TopQuestionSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      color: Colors.blueGrey[900],
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Expanded(
-            flex: 7,
-            child: QuestionTile(),
-          ),
-          Expanded(
-            flex: 1,
-            child: AnswerTile.Builder(Colors.red[500], 'Negative'),
-          ),
-          Expanded(
-            flex: 1,
-            child: AnswerTile.Builder(Colors.lightGreen[900], 'Positive'),
-          ),
-        ],
-      ),
+      color: Colors.white,
+      child: QuestionTile.builder(
+          'This is a sample text which keeps on growing like trees all round the year, all through the village but never ceases to exist.'),
+    );
+  }
+}
+
+class BottomAnswerSection extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _BottomAnswerSection();
+}
+
+class _BottomAnswerSection extends State<BottomAnswerSection> {
+  // List<String> lsResponseOptions = List.empty(growable: true);
+  List<String> lsResponseOptions = ['Accept', 'Deny'];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black54,
+      child: AnswerTile.Builder(lsResponseOptions),
     );
   }
 }
