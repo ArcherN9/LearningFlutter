19d5d3406c54bd20e24894f463415f7890992bc2
ArcherN9 <>
2021-03-11 21:03:03 +0530

    Rationale:
    1. Modified QuestionTile to a StatefulWidget which accepts question
       strings through the constructor

--- Quizzler/lib/Widgets/QuestionTile.dart
+++ Quizzler/lib/Widgets/QuestionTile.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/material.dart';
 
-class QuestionTile extends StatelessWidget {
+class QuestionTile extends StatefulWidget {
+  // The question to be displayed to the user
+  String strQuestion;
+
+  QuestionTile.builder(this.strQuestion);
+
+  @override
+  State<StatefulWidget> createState() => _QuestionTile(strQuestion);
+}
+
+class _QuestionTile extends State<QuestionTile> {
+  // The question to be displayed to the user
+  String strQuestion;
+
+  _QuestionTile(this.strQuestion);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text(
-          'This is a sample text which keeps on growing like trees all round the year, all through the village but never ceases to exist.',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 22,
-            fontFamily: 'UbuntuMono',
+        child: Padding(
+          padding: EdgeInsets.only(left: 25, right: 25),
+          child: Text(
+            this.strQuestion,
+            style: TextStyle(
+              color: Colors.black87,
+              fontWeight: FontWeight.bold,
+              fontSize: 22,
+              fontFamily: 'Montserrat',
+            ),
+            softWrap: true,
+            textAlign: TextAlign.left,
           ),
-          softWrap: true,
-          textAlign: TextAlign.center,
         ),
       ),
     );
